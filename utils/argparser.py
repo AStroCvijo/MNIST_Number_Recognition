2421f22a146935753446ff83509b1fe9d2a941c4
Jovan Cvijanović <119356876+AStroCvijo@users.noreply.github.com>
2024-08-19 15:05:48 +0200

    Update argparser.py

--- utils/argparser.py
+++ utils/argparser.py
@@ -6,9 +6,9 @@ def arg_parse():
     parser = argparse.ArgumentParser()
 
     # Training arguments
-    parser.add_argument('-t', "--train", dest="train",action="store_true", help="Weather to train the CNN")
-    parser.add_argument('-e',  '--epochs',               type=int,   required=False, default = 5, help="Number of epochs for training")
-    parser.add_argument('-lr', '--learning_rate',        type=float, required=False, default = 0.001, help="Learning rate for training")
+    parser.add_argument('-t', "--train",          action="store_true", default = True, help="Weather to train the CNN")
+    parser.add_argument('-e',  '--epochs',        type=int,   required=False, default = 2, help="Number of epochs for training")
+    parser.add_argument('-lr', '--learning_rate', type=float, required=False, default = 0.001, help="Learning rate for training")
 
     # Parse the arguments
     return parser.parse_args()
